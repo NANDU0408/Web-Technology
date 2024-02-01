4b44f147698604ff1697c0dbd9fff1f1c1980df3
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2024-02-01 23:28:29 +0530

    Web Technology

---
+++ JavaScript.js
@@ -0,0 +1,29 @@
+var ref;
+ref = 21;
+console.log(ref, typeof(ref));
+
+let city; 
+city = "Dehli";
+console.log(city, typeof(city));
+
+const ref2 = "Sun";
+console.log(ref2, typeof(ref2));
+
+(function(){//Self Invoking
+    var ref3 =50;
+    console.log(ref3)
+}())
+
+//Named Function
+function javascript(){
+    console.log("Named function");
+}
+javascript();
+
+//Arrow Function
+var arrowFunction=()=>{
+    console.log("This is arrow function")
+}
+arrowFunction();
+
+//TASK:-take 5 examples try to write 10 var let const console print 10 functions
---
+++ task.js
@@ -0,0 +1,267 @@
+var doorManufactureCompany = "techFurnitures";
+const color = "wooden";
+let quantity = 5;
+var length = 6;
+const openDirection = "left";
+let width = 10;
+var lockSystem = "casual"
+const cost = 5500;
+var material = "wood";
+let checkSafety = true;
+
+console.log("Door Manufacture Company:", doorManufactureCompany);
+console.log("Color:", color);
+console.log("Quantity:", quantity);
+console.log("Length:", length);
+console.log("Open Direction:", openDirection);
+console.log("Width:", width);
+console.log("Lock System:", lockSystem);
+console.log("Cost:", cost);
+console.log("Material:", material);
+console.log("Check Safety:", checkSafety);
+
+var doorManufactureCompany = "Furnituristics";
+const outerColor = "green";
+let quantityIn = 5;
+var length = 4;
+const openingDirection = "right";
+let size = 10;
+var lockSystem = "digital"
+const price = 7500;
+var material = "titanium";
+let isSafe = true;
+
+console.log("Door Manufacture Company:", doorManufactureCompany);
+console.log("Outer Color:", outerColor);
+console.log("Quantity:", quantityIn);
+console.log("Length:", length);
+console.log("Opening Direction:", openingDirection);
+console.log("Size:", size);
+console.log("Lock System:", lockSystem);
+console.log("Price:", price);
+console.log("Material:", material);
+console.log("Is Safe:", isSafe);
+
+var mouseManufactureCompany = "TechGadgets";
+const mouseColor = "black";
+var length = 4; 
+let widthOfMouse = 2.5; 
+var connectionType = "wireless"; 
+var sensorType = "optical"; 
+const mouseCost = 350; 
+var material = "plastic"; 
+let programmableButtons = 5; 
+var ergonomicDesign = true;
+
+console.log("Mouse Manufacture Company:", mouseManufactureCompany);
+console.log("Mouse Color:", mouseColor);
+console.log("Length of Mouse:", length);
+console.log("Width of Mouse:", widthOfMouse);
+console.log("Connection Type:", connectionType);
+console.log("Sensor Type:", sensorType);
+console.log("Mouse Cost:", mouseCost);
+console.log("Material:", material);
+console.log("Programmable Buttons:", programmableButtons);
+console.log("Ergonomic Design:", ergonomicDesign);
+
+var plugManufactureCompany = "PowerTech";
+const plugColor = "white";
+var length = 2.5; 
+let plugWidth = 1.5;
+var plugType = "Type A"; 
+const voltage = 120; 
+const plugCost = 10; 
+var material = "plastic";
+let hasGrounding = true; 
+var surgeProtection = false;
+
+console.log("Plug Manufacture Company:", plugManufactureCompany);
+console.log("Plug Color:", plugColor);
+console.log("Length of Plug:", length);
+console.log("Width of Plug:", plugWidth);
+console.log("Plug Type:", plugType);
+console.log("Voltage:", voltage);
+console.log("Plug Cost:", plugCost);
+console.log("Material:", material);
+console.log("Has Grounding:", hasGrounding);
+console.log("Surge Protection:", surgeProtection);
+
+var tableManufactureCompany = "FurnitureCraft";
+var length = 48; 
+var tableWidth = 30; 
+const height = 29; 
+var material = "wood"; 
+const shape = "rectangular"; 
+var legs = 4; 
+var hasDrawer = true; 
+const tableCost = 150; 
+let isFoldable = false;
+
+console.log("Table Manufacture Company:", tableManufactureCompany);
+console.log("Length of Table:", length);
+console.log("Width of Table:", tableWidth);
+console.log("Height of Table:", height);
+console.log("Material:", material);
+console.log("Shape of Table:", shape);
+console.log("Number of Legs:", legs);
+console.log("Has Drawer:", hasDrawer);
+console.log("Table Cost:", tableCost);
+console.log("Is Foldable:", isFoldable);
+
+//With Functions
+
+(function () {
+    var universityName = "Tech University";
+    let noOfProfessors = 100;
+    const location = "Tech City";
+    console.log(universityName);
+    console.log(noOfProfessors);
+    console.log(location);
+}())
+
+function universityLocation() {
+    var place = "Tech University Campus";
+    let city = "Tech City";
+    const state = "Tech State";
+    console.log(place);
+    console.log(city);
+    console.log(state);
+}
+
+universityLocation();
+
+var departmentDetails = () => {
+    var departmentName = "Computer Science";
+    let coursesOffered = ["Computer Science", "Information Technology"];
+    const noOfStudents = 500;
+    console.log(departmentName);
+    console.log(coursesOffered);
+    console.log(noOfStudents);
+}
+
+departmentDetails();
+
+
+(function () {
+    var libraryName = "Tech Library";
+    let noOfBooks = 5000;
+    const location = "Tech Town";
+    console.log(libraryName);
+    console.log(noOfBooks);
+    console.log(location);
+}())
+
+function libraryLocation() {
+    var branch = "Tech Library Branch";
+    let city = "Tech City";
+    const state = "Tech State";
+    console.log(branch);
+    console.log(city);
+    console.log(state);
+}
+
+libraryLocation();
+
+var bookDetails = () => {
+    var genre = "Science Fiction";
+    let popularAuthors = ["Isaac Asimov", "Arthur C. Clarke"];
+    const availableCopies = 1000;
+    console.log(genre);
+    console.log(popularAuthors);
+    console.log(availableCopies);
+}
+bookDetails();
+
+
+(function () {
+    var coffeeShopName = "Tech Brews";
+    let noOfBaristas = 10;
+    const location = "Tech Plaza";
+    console.log(coffeeShopName);
+    console.log(noOfBaristas);
+    console.log(location);
+}())
+
+function coffeeShopLocation() {
+    var branch = "Tech Brews Branch";
+    let city = "Tech City";
+    const state = "Tech State";
+    console.log(branch);
+    console.log(city);
+    console.log(state);
+}
+
+coffeeShopLocation();
+
+var coffeeMenu = () => {
+    var popularBlend = "Tech Signature";
+    let availableSizes = ["Small", "Medium", "Large"];
+    const priceRange = "$3 - $6";
+    console.log(popularBlend);
+    console.log(availableSizes);
+    console.log(priceRange);
+}
+coffeeMenu();
+
+
+(function () {
+    var dealershipName = "Tech Motors";
+    let noOfSalespeople = 15;
+    const location = "Tech Auto Center";
+    console.log(dealershipName);
+    console.log(noOfSalespeople);
+    console.log(location);
+}())
+
+function dealershipLocation() {
+    var branch = "Tech Motors Branch";
+    let city = "Tech City";
+    const state = "Tech State";
+    console.log(branch);
+    console.log(city);
+    console.log(state);
+}
+
+dealershipLocation();
+
+var carModels = () => {
+    var popularBrand = "TechCars";
+    let availableModels = ["Sedan", "SUV", "Electric"];
+    const warrantyPeriod = "3 years";
+    console.log(popularBrand);
+    console.log(availableModels);
+    console.log(warrantyPeriod);
+}
+carModels();
+
+
+(function () {
+    var theaterName = "Tech Cinemas";
+    let noOfScreens = 8;
+    const location = "Tech Movieplex";
+    console.log(theaterName);
+    console.log(noOfScreens);
+    console.log(location);
+}())
+
+function theaterLocation() {
+    var branch = "Tech Cinemas Branch";
+    let city = "Tech City";
+    const state = "Tech State";
+    console.log(branch);
+    console.log(city);
+    console.log(state);
+}
+
+theaterLocation();
+
+var movieSchedule = () => {
+    var currentBlockbuster = "Tech Blockbuster";
+    let screeningTimes = ["1:00 PM", "4:00 PM", "7:00 PM"];
+    const ticketPrice = "$12";
+    console.log(currentBlockbuster);
+    console.log(screeningTimes);
+    console.log(ticketPrice);
+}
+movieSchedule();
+
